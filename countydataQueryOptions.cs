cf145525bd5c1fbcbe33b994f10a8278aea6dcd9
APIVerve Team <hello@apiverve.com>
2024-05-14 15:24:02 -0500

    Bug fixes and Improvements

---
+++ countydataQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class countydataQueryOptions {
+public string state { get; set; }
+public string county { get; set; }
+}
+
+}
